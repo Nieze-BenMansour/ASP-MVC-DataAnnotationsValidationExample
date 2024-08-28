dbf5c578587a8a9ce36fdc18f7d901f0cf4f4d3e
Nieze Ben Mansour <nieze.benmansour@outlook.fr>
2024-08-28 10:20:52 +0100

    Add project files.

---
+++ DataAnnotationsExample/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DataAnnotationsExample.Models;
+using DataAnnotationsExample.Validators;
+
+namespace DataAnnotationsExample.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Details(
+        Person person,
+        [FromServices] PersonValidator personValidator)
+    {
+        //Business validation
+        var validationDict = personValidator.Validate(person);
+
+        PopulateModelState(validationDict);
+
+        if (!ModelState.IsValid)
+        {
+            return View("Index", person);
+        }
+        return View(person);
+    }
+
+    private void PopulateModelState(Dictionary<string, string> validationDict)
+    {
+        foreach (var validationKeyValue in validationDict)
+        {
+            ModelState.AddModelError(validationKeyValue.Key, validationKeyValue.Value);
+        }
+    }
+}
---
+++ DataAnnotationsExample/Validators/PersonValidator.cs
@@ -0,0 +1,25 @@
+﻿using DataAnnotationsExample.Models;
+
+namespace DataAnnotationsExample.Validators;
+
+public class PersonValidator
+{
+    private const int _realMinValueAge = 6;
+
+    public Dictionary<string, string> Validate(Person person)
+    {
+        var validationDict = new Dictionary<string, string>();
+
+        if (string.IsNullOrEmpty(person.FirstName) && string.IsNullOrEmpty(person.LastName))
+        {
+            validationDict.Add("PersonRule", "Ce n'est logique !!");
+        }
+
+        if (person.Age == _realMinValueAge)
+        {
+            validationDict.Add("AgePersonRule", "Trop petit !!");
+        }
+
+        return validationDict;
+    }
+}
